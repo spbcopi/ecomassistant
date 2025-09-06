6679f2c965c2c7d78d28df24e642bb887db6c40f
Sampath Kumar <sampathkumarpb@gmail.com>
2025-09-06 14:12:35 +0530

    fixing model loaders

--- assistant/utils/model_loader.py
+++ assistant/utils/model_loader.py
@@ -1,13 +1,14 @@
 import os
 import sys
+import asyncio
 from dotenv import load_dotenv
 from assistant.utils.config_loader import load_config
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
 from assistant.logger import GLOBAL_LOGGER as log
 from assistant.exception.custom_exception import ProductAssistantException
-import asyncio
 from api_key_manager import ApiKeyMgr
+from langchain.chat_models import ChatOpenAI
 
 class ModelLoader:
     """
@@ -44,3 +45,52 @@ class ModelLoader:
         except Exception as e:
             log.error("Failed to get embedding model config", error=str(e))
             raise ProductAssistantException("Invalid embedding model config", sys)
+        
+    def load_llm(self):
+        """
+        Load and return the configured LLM model.
+        """
+        llm_block = self.config["llm"]
+        provider_key = os.getenv("LLM_PROVIDER", "google")
+
+        if provider_key not in llm_block:
+            log.error("LLM provider not found in config", provider=provider_key)
+            raise ValueError(f"LLM provider '{provider_key}' not found in config")
+
+        llm_config = llm_block[provider_key]
+        provider = llm_config.get("provider")
+        model_name = llm_config.get("model_name")
+        temperature = llm_config.get("temperature", 0.2)
+        max_tokens = llm_config.get("max_output_tokens", 2048)
+    
+        log.info("Loading LLM", provider=provider, model=model_name)
+
+        match provider:
+            case "google":
+                return ChatGoogleGenerativeAI(
+                    model=model_name,
+                    google_api_key=self.api_key_mgr.get("GOOGLE_API_KEY"),
+                    temperature=temperature,
+                    max_output_tokens=max_tokens
+                )
+            case "groq":
+                return ChatGroq(
+                    model=model_name,
+                    api_key=self.api_key_mgr.get("GROQ_API_KEY"), #type: ignore
+                    temperature=temperature,
+                )           
+            case "openai":
+                return ChatOpenAI(
+                    model_name=model_name,
+                    temperature=temperature,
+                    max_tokens=max_tokens,
+                    openai_api_key=self.api_key_mgr.get("OPENAI_API_KEY")
+                )
+            case _:
+                log.error("Unsupported LLM provider", provider=provider)
+                raise ValueError(f"Unsupported LLM provider: {provider}")
+
+            
+
+
+
